554c15f869d225d55e881c3be12e8842b3e13740
Gabe Wyatt <gabe@solarixdigital.com>
2021-01-27 21:25:28 -0800

    feat: add email transports
    Uses mailtrap.io for local development and sendmail for test environment.
    
    Refs #1
    
    feat: add customizable email templates
    
    Adjusted a handful of views and translation strings, but otherwise templates are
     default.
    
    ci: add package installation for submodule projects into deploy stage

--- .deploy/all/deploy.sh
+++ .deploy/all/deploy.sh
@@ -24,6 +24,15 @@ EOF
 echo "Copying ${ENV_FILE} to .env."
 scp -o StrictHostKeyChecking=no "${ENV_FILE}" "${REMOTE_USER}@${DEPLOY_ENDPOINT}:${TARGET_DIRECTORY}.env"
 
+# Install submodule packages
+ssh -o StrictHostKeyChecking=no "${REMOTE_USER}"@"${DEPLOY_ENDPOINT}" << EOF
+  echo "Installing submodule Composer packages"
+  cd "${TARGET_DIRECTORY}/packages/solarix/sylius-authorize-net-plugin"
+  composer install
+  cd "${TARGET_DIRECTORY}/packages/solarix/shipping"
+  composer install
+EOF
+
 # Install Composer packages, Node packages, and rebuild
 ssh -o StrictHostKeyChecking=no "${REMOTE_USER}"@"${DEPLOY_ENDPOINT}" << EOF
   cd ${TARGET_DIRECTORY}
